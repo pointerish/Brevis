50180796631be5b2bca7e1797b685768d2b0ea61
Josias Alvarado <josiasjag@gmail.com>
2020-08-15 06:39:01 -0600

    Added extra route for /

--- brevis/api.py
+++ brevis/api.py
@@ -21,3 +21,15 @@ def redirect_to_url(url_id):
         urls = json.load(jf)
     original_url = urls[url_id]
     return redirect(original_url)
+
+@app.route('/')
+def root():
+    html = '''
+    <h1>Brevis URL Shortener</h1>
+    <p>Brevis accepts a POST request containing a JSON formatted as such:<li>{"url":"Long URL goes here."}</li>
+    This POST request will return a json containing the id of the URL and the client should then send a GET request to
+    https://brevis-shortener.herokuapp.com/<URLID> which will redirect to the original URL.</p>
+    <p> Of course, the Heroku domain is long and beats the purpose of the shortener, but this is just a proof of concept and a learning 
+    project.</p> 
+    '''
+    return html
